59d6ca80d6e0bb5e69a6084237d1582aad02cbe2
felipefariasdasilva <feliipefarias@outlook.com>
2020-03-05 20:58:56 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import './App.css';
+import Tabela from './Tabela.js'
+
+class App extends Component {
+
+  state = {
+    autores : [
+      {
+        nome: 'Paulo',
+        livro: 'React',
+        preco: '1000'
+      },
+      {
+        nome: 'Daniel',
+        livro: 'Java',
+        preco: '99'
+      },
+      {
+        nome: 'Marcos',
+        livro: 'Design',
+        preco: '150'
+      },
+      {
+        nome: 'Bruno',
+        livro: 'DevOps',
+        preco: '100'
+      },
+      {
+        nome: 'Nico',
+        livro: 'Python',
+        preco: '333'
+      }
+    ],
+
+  };
+
+  removeAutor = () =>{
+
+    const { autores } = this.state; 
+    this.setState(
+      {
+        autores : autores.filter((autor, posAtual) => {
+          console.log(autor, posAtual);
+          return posAtual;
+        }),
+      }
+    );
+  }
+
+  render(){
+    return (
+      <div className="App">
+  
+        <Tabela autores = { this.state.autores } removeAutor = {this.removeAutor} />
+  
+      </div>
+    );
+  }
+  
+}
+
+export default App;
---
+++ src/Tabela.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+const TableHead = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Autores</th>
+                <th>Livros</th>
+                <th>Preços</th>
+                <th>Remover</th>
+            </tr>
+        </thead>
+    );
+}
+
+const TableBody = props => {
+
+    const linhas = props.autores.map((linha, index) => {
+        return(
+            <tr key={index}>
+                <td>{ linha.nome }</td>
+                <td>{ linha.livro }</td>
+                <td>{ linha.preco }</td>
+                <td><button>Remover</button></td>
+            </tr>
+        );
+    });
+
+    return(
+        <tbody>
+            {linhas}
+        </tbody>
+    );
+}
+
+class Tabela extends Component {
+
+    render() {
+        
+        const{ autores } = this.props;
+        console.log(autores);
+
+        return (
+            <table>
+                <TableHead />
+                <TableBody autores = { autores }/>
+            </table>
+        );
+    }
+
+}
+
+export default Tabela;
